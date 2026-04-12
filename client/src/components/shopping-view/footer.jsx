6597795ef9ab930c2c3fc1b593d1ad18c78f7e7b
Abid Azam Khan <abidazamkhan26@gmail.com>
2026-04-12 08:08:23 +0600

    update link

--- client/src/components/shopping-view/footer.jsx
+++ client/src/components/shopping-view/footer.jsx
@@ -5,26 +5,9 @@ function ShoppingFooter() {
     <footer className="border-t bg-white py-6">
       <div className="container mx-auto px-4 md:px-6">
         <div className="flex flex-col items-center gap-4 text-center">
-          <p className="text-sm text-gray-600 animate-in fade-in duration-700">
-            &copy; 2026 ProShop E-com Platform. Developed By{" "}
-            <a
-              href="https://abidazam.com"
-              target="_blank"
-              rel="noreferrer"
-              className="font-semibold text-black hover:underline"
-            >
-              Abid Azam Khan
-            </a>{" "}
-            · CEO of{" "}
-            <a
-              href="https://unipixer.com"
-              target="_blank"
-              rel="noreferrer"
-              className="font-semibold text-black hover:underline"
-            >
-              Unipixer
-            </a>
-          </p>
+           <p className="py-4 text-center text-xs md:text-sm">
+        © 2026 Gadgety. Developed By <span href="" target="_blank" className="font-medium ">Abid Azam Khan</span> · CEO of <span href="" target="_blank" className="font-medium">Unipixer</span>
+      </p>
         </div>
       </div>
     </footer>
